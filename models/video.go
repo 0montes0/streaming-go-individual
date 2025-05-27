4a90525e9475b64cbdd28961acf9c0739f889b4d
0montes0 <andresmontes045@gmail.com>
2025-05-26 23:19:04 -0500

    Update video.go

--- models/video.go
+++ models/video.go
@@ -6,3 +6,6 @@ type Video struct {
     Category string
     FilePath string
 }
+var Videos = []Video{
+    {ID: 1, Title: "Go Tutorial", Category: "Tutorial", FilePath: "videos/go_tutorial.mp4"},
+    {ID: 2, Title: "Nature", Category: "Documental", FilePath: "videos/nature.mp4"},
